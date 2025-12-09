40be569d059a1931ab1a81a2e1b01b954d5e9f28
VedSharma9644 <58023735+VedSharma9644@users.noreply.github.com>
2025-12-09 16:52:33 +0530

    Fix duplicate React import

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './navigation/AppNavigator';
 import { StatusBar } from 'expo-status-bar';
